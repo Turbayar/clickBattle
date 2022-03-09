9c9226c9e310b303e7428ca7dcded1a9d5a512fe
Turbayar <Tito@Bilguuns-iMac.local>
2022-03-09 13:44:49 +0800

    ract-version battle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import {useState} from "react";
 function App() {
+  const [state, setState] = useState({
+    count: 50,
+    count2: 50,
+  })
+  
+let {count, count2} = state;
+
+const clickField1 = () => {
+  setState({...state, count: count+1})
+}
+
+const clickField2 = () => {
+  setState({...state, count2: count2+1})
+}
+
+
+const clickButton = () => {
+  
+}
+
+
+let red = (count*100)/(count + count2);
+let blue = (count2*100)/(count+count2);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+      <h1 className="score-1">  {count} </h1>
+        <div className="reset"> <button onClick={clickButton}> reset </button> </div>
+        <h1 className="score-2"> {count2} </h1>
+      </div>
+      <div className="main"> 
+        <div className="red-team" onClick={clickField1} style={{width: `${red}%`}}></div>
+        <div className="blue-team" onClick={clickField2} style={{width: `${blue}%`}}></div>
+    </div>
+      
     </div>
   );
 }
